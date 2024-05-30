14ca2f366885b80963e83465e3905cb899c71216
Георгий Щербаков <soulvayne@MacBook-Pro-Georgij.local>
2024-05-30 15:53:26 +0300

    step 2

--- gendiff.js
+++ gendiff.js
@@ -5,5 +5,14 @@ const { program } = require('commander');
 program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format')
   .helpOption('-h, --help', 'output usage information')
   .parse(process.argv);
+
+const options = program.opts();
+const [filepath1, filepath2] = program.args;
+
+console.log('Filepath1:', filepath1);
+console.log('Filepath2:', filepath2);
+console.log('Format:', options.format);
