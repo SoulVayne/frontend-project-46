6964280086583244674ba1d75b8b044811aa16e3
Георгий Щербаков <soulvayne@MacBook-Pro-Georgij.local>
2024-05-30 16:10:31 +0300

    step 3

--- gendiff.js
+++ gendiff.js
@@ -1,18 +1,29 @@
 #!/usr/bin/env node
 
+const fs = require('fs');
+const path = require('path');
 const { program } = require('commander');
+
+const parseFile = (filepath) => {
+  const absolutePath = path.resolve(process.cwd(), filepath);
+  const fileContent = fs.readFileSync(absolutePath, 'utf-8');
+  return JSON.parse(fileContent);
+};
+
+const genDiff = (filepath1, filepath2) => {
+  const data1 = parseFile(filepath1);
+  const data2 = parseFile(filepath2);
+
+  console.log('File 1 data:', data1);
+  console.log('File 2 data:', data2);
+};
 
 program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format [type]', 'output format')
-  .helpOption('-h, --help', 'output usage information')
+  .action((filepath1, filepath2) => {
+    genDiff(filepath1, filepath2);
+  })
   .parse(process.argv);
-
-const options = program.opts();
-const [filepath1, filepath2] = program.args;
-
-console.log('Filepath1:', filepath1);
-console.log('Filepath2:', filepath2);
-console.log('Format:', options.format);
